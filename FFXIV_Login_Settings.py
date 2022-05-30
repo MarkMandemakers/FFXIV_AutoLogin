5f465089405c200f229e19c2f61cc88e32350f09
Mark Mandemakers <803451+MarkMandemakers@users.noreply.github.com>
2022-05-30 12:48:33 +0200

    Update settings file to reflect 2FA addition

--- FFXIV_Login_Settings.py
+++ FFXIV_Login_Settings.py
@@ -1,6 +1,7 @@
 # Fill in settings here
 launcher_loc = "LAUNCHER LOCATION"
 password = "FFXIV ACCOUNT PASSWORD"
+token_2fa = "AUTHENTICATOR TOKEN SECRET"
 
 # Example of launcher_loc:
 # [Steam]    "steam://rungameid/39210"
